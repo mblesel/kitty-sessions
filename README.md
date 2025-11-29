4def3a1de82df1738949b3a34aa42efb192833e4
Michael Blesel <michael.blesel@ovgu.de>
2025-11-29 16:36:04 +0100

    inital script version

---
+++ README.md
@@ -0,0 +1,44 @@
+# kitty-sessions
+
+A simple bash script for managing kitty sessions in a similar way to `tmux-sessionizer` (or as close as I could get).
+The script allows choosing a project directory via `fzf` and starting a kitty session for it.
+It also contains a save function that allows to overwrite a running session with the current state.
+Saving is not automatic yet and needs to be triggered manually.
+
+## How it works
+
+For each session a `<project-name>.kitty-session` file is created in the project directory.
+If that file already exists the script will load the session.
+Otherwise it will create a new 'empty' session with just one tab in the project's main directory.
+
+## Requirements
+
+The script needs `fzf` and `fd` installed on your system.
+It also requires `allow_remote_control yes` to be activated in your kitty config.
+
+## Usage
+
+Download the script and put it somewhere.
+
+Replace the following line in the script with paths to your own project directories.
+
+```bash
+# Set your Project paths here
+# Currently only a depth of 1 is supported
+KS_PATHS=(~/Projects ~/Documents)
+```
+
+Add the following mappings to your kitty config (replacing the path to the script with where you put it).
+
+```bash
+# Run the interactive fzf picker to start/load a session
+map f5 launch --type=overlay-main ~/.local/bin/scripts/kitty-sessions.sh start
+# Save the current session
+map f6 launch --type=tab --tab-title=KS_TAB --add-to-session . --keep-focus ~/.local/bin/scripts/kitty-sessions.sh save
+```
+
+## Remarks
+
+This is mainly for personal use and not very polished yet.
+The script needs some not so nice workarounds to make kitty sessions behave in the way that I want them to.
+Very likely there are some bugs and use cases I didn't think of.
---
+++ kitty-sessions.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+# Set your Project paths here
+# Currently only a depth of 1 is supported
+KS_PATHS=(~/Projects ~/Documents)
+
+# Check if all requirements are installed
+sanity_check() {
+    if ! command -v fzf &>/dev/null; then
+        echo "fzf is not installed. Please install it first."
+        exit 1
+    fi
+    if ! command -v fd &>/dev/null; then
+        echo "fd is not installed. Please install it first."
+        exit 1
+    fi
+}
+
+usage() {
+    echo "Usage: $0 [start|save]"
+}
+
+# Load an existing session or create a new one if the project does not already have one
+start_session() {
+    # Get all directories that already contain a kitty session file
+    SESSION_FILES=$(fd --glob '*.kitty-session' "${KS_PATHS[@]}")
+    SESSION_DIRS=$(echo "$SESSION_FILES" | while read -r session; do
+        session_name=${session%/*.kitty-session}
+        echo "$session_name"
+    done)
+
+    # Get all subdirectories from the given paths in KS_PATHS
+    DIRS=$(fd . -t d --max-depth=1 "${KS_PATHS[@]}")
+    # Remove all paths that already contain a kitty session file
+    if [[ -n "$SESSION_DIRS" ]]; then
+        FILTERED_DIRS=$(echo "$DIRS" | grep -v -F -f <(echo "$SESSION_DIRS"))
+        # Tell the user about existing sessions by adding the [KITTY] prefix to them
+        DISPLAY_SESSIONS=$(echo "$SESSION_DIRS" | while read -r session; do
+            echo "[KITTY] $session"
+        done)
+    else
+        FILTERED_DIRS="$DIRS"
+        DISPLAY_SESSIONS=""
+    fi
+
+    SELECTED=$({
+        echo "${DISPLAY_SESSIONS}"
+        echo "${FILTERED_DIRS}"
+    } | fzf)
+
+    if [[ -n "$SELECTED" ]]; then
+        # If an already exsiting session file was selected this session is loaded
+        # KS_CURRENT_SESSION is set to the path of the selected session file
+        # This is required for the kitty-save-session.sh script to work
+        if [[ "$SELECTED" == "[KITTY] "* ]]; then
+            ORIGINAL_PATH="${SELECTED#\[KITTY\] }"
+            PROJECT_NAME="${SELECTED##*/}"
+            ORIGINAL_PATH="${ORIGINAL_PATH}/${PROJECT_NAME}.kitty-session"
+            kitten @ env KS_CURRENT_SESSION="$ORIGINAL_PATH"
+            kitten @ action goto_session "$ORIGINAL_PATH"
+        else
+            SESSION_FILE_PATH="${SELECTED%/}"
+            PROJECT_NAME="${SESSION_FILE_PATH##*/}"
+            SESSION_FILE_PATH="${SESSION_FILE_PATH}/${PROJECT_NAME}.kitty-session"
+            echo "${SESSION_FILE_PATH}" >~/out
+            kitten @ env KS_CURRENT_SESSION="${SESSION_FILE_PATH}"
+            kitten @ action launch --title="${PROJECT_NAME}" --type=tab --tab-title="${PROJECT_NAME}" --cwd="${SELECTED}"
+            kitten @ action save_as_session --relocatable --match=session:^$ --save-only "${SESSION_FILE_PATH}"
+            cp "${SESSION_FILE_PATH}" "${SESSION_FILE_PATH}.tmp"
+            sed -n "/new_tab ${PROJECT_NAME}/,/focus/p" "${SESSION_FILE_PATH}.tmp" >"${SESSION_FILE_PATH}"
+            rm "${SESSION_FILE_PATH}.tmp"
+            kitten @ close-tab --match=title:"${PROJECT_NAME}"
+            kitten @ action goto_session "${SESSION_FILE_PATH}"
+        fi
+    fi
+}
+
+# save the session of the currently active tab
+save_session() {
+    if [ -n "${KS_CURRENT_SESSION}" ]; then
+        kitten @ action save_as_session --relocatable --save-only --match=session:. "${KS_CURRENT_SESSION}"
+        # This workaround uses a new tab to run the script which has the name KS_TAB
+        # This tab is then removed from the session file again
+        sed -i '/new_tab KS_TAB/,/focus/d' "${KS_CURRENT_SESSION}"
+    fi
+}
+
+sanity_check
+
+if [ $# -gt 0 ]; then
+    case "$1" in
+    start)
+        start_session
+        ;;
+    save)
+        save_session
+        ;;
+    -h)
+        usage
+        ;;
+    *)
+        echo "Unknown argument: $1"
+        exit 1
+        ;;
+    esac
+else
+    usage
+fi
+
+## TODOS
+# Add config file where you can set the project directories being searched
+# Look into automatic session saving
